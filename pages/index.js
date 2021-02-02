5133a04d920345391a9b7a21f23eea36e8debda4
ricardoelias84 <ricardoelias@gmail.com>
2021-02-02 00:26:24 -0300

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function home() {
+    return <div align="center">
+        <h1>Em desenvolvimento</h1>
+        <p><h2>RicardoElias.com</h2></p>
+        </div>
+}
+
+export default home
