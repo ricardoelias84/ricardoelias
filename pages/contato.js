081a6748a2aeb9a577248e339b0b0133b33e3331
ricardoelias84 <ricardoelias@gmail.com>
2021-02-05 07:38:50 -0300

    com novo projeto

--- pages/contato.js
+++ pages/contato.js
@@ -3,7 +3,9 @@ import Link from 'next/link';
 function contato() {
     return <div align="center">
         <h1>Contato</h1>
-        <p><h2>se precisar falar comigo é só preencher esse formulario, ou clique aqui e me manda uma mensagem no whatsapp</h2></p>
+        <p><h2>Se precisar falar comigo é só preencher esse formulario, ou clique aqui e me manda uma mensagem no <Link href="https://api.whatsapp.com/send?phone=5515981790411&text=Mensagem%20atrav%C3%A9s%20do%20site">whatsapp</Link>
+            
+            </h2></p>
             <p>
                 <h3>
             <Link href="/">Home</Link> |
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
-import { home } from './home';
+import Link from 'next/link';
 
+export function home() {
+    return <div align="center">
+        <h1>Sejam bem vindos</h1>
+        <p><h2>ricardoelias.com.br em desenvolvimento</h2></p>
+        <p>
+            <h3>
+            <Link href="/">Home</Link> |
+            <Link href="/sobre">Sobre</Link> |
+            <Link href="/servicos">Serviços</Link> |
+            <Link href="/blog">Blog</Link> |
+            <Link href="/depoimentos">Depoimentos</Link> |
+            <Link href="/contato">Contato</Link>
+            </h3>
+        </p>
+    </div>;
+
+}
 export default home
