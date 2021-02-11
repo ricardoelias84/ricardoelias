b53cd2851af0795eea48d7af9d561c0d7e1487be
ricardoelias84 <ricardoelias@gmail.com>
2021-02-11 14:42:56 -0300

    11.02 14h42

--- pages/sobre.js
+++ pages/sobre.js
@@ -3,80 +3,97 @@ import React from "react";
 import classNames from "classnames";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-
 // @material-ui/icons
-
+import Camera from "@material-ui/icons/Camera";
+import Palette from "@material-ui/icons/Palette";
+import Favorite from "@material-ui/icons/Favorite";
 // core components
 import Header from "components/Header/Header.js";
 import Footer from "components/Footer/Footer.js";
+import Button from "components/CustomButtons/Button.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
 import HeaderLinks from "components/Header/HeaderLinks.js";
+import NavPills from "components/NavPills/NavPills.js";
 import Parallax from "components/Parallax/Parallax.js";
 
-import styles from "assets/jss/nextjs-material-kit/pages/landingPage.js";
+import profile from "assets/img/faces/elias.jpg";
 
-// Sections for this page
-import ProductSection from "pages-sections/LandingPage-Sections/ProductSection.js";
-import TeamSection from "pages-sections/LandingPage-Sections/TeamSection.js";
-import WorkSection from "pages-sections/LandingPage-Sections/WorkSection.js";
+import studio1 from "assets/img/examples/studio-1.jpg";
+import studio2 from "assets/img/examples/studio-2.jpg";
+import studio3 from "assets/img/examples/studio-3.jpg";
+import studio4 from "assets/img/examples/studio-4.jpg";
+import studio5 from "assets/img/examples/studio-5.jpg";
+import work1 from "assets/img/examples/olu-eletu.jpg";
+import work2 from "assets/img/examples/clem-onojeghuo.jpg";
+import work3 from "assets/img/examples/cynthia-del-rio.jpg";
+import work4 from "assets/img/examples/mariya-georgieva.jpg";
+import work5 from "assets/img/examples/clem-onojegaw.jpg";
 
-const dashboardRoutes = [];
+import styles from "assets/jss/nextjs-material-kit/pages/profilePage.js";
 
 const useStyles = makeStyles(styles);
 
-export default function LandingPage(props) {
+export default function ProfilePage(props) {
   const classes = useStyles();
   const { ...rest } = props;
+  const imageClasses = classNames(
+    classes.imgRaised,
+    classes.imgRoundedCircle,
+    classes.imgFluid
+  );
+  const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
   return (
     <div>
       <Header
         color="transparent"
-        routes={dashboardRoutes}
-        brand="Ricardo Elias"
+        brand="NextJS Material Kit"
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
-          height: 400,
+          height: 200,
           color: "white"
         }}
         {...rest}
       />
-      <Parallax filter responsive image={require("assets/img/landing-bg.jpg")}>
-        <div className={classes.container}>
-          <GridContainer>
-            <GridItem xs={12} sm={12} md={6}>
-              <h1 className={classes.title}>Sobre</h1>
-              <h4>
+      <Parallax small filter image={require("assets/img/profile-bg.jpg")} />
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div>
+          <div className={classes.container}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={6}>
+                <div className={classes.profile}>
+                  <div>
+                    <img src={profile} alt="..." className={imageClasses} />
+                  </div>
+                  <div className={classes.name}>
+                    <h3 className={classes.title}>Ricardo Elias</h3>
+                    <h6>Proteção de dados</h6>
+                    <Button justIcon link className={classes.margin5}>
+                      <i className={"fab fa-instagram"} />
+                    </Button>
+                    <Button justIcon link className={classes.margin5}>
+                      <i className={"fab fa-facebook"} />
+                    </Button>
+                  </div>
+                </div>
+              </GridItem>
+            </GridContainer>
+            <div className={classes.description}>
+              <p>
               Bacharelado em Direito com Pós Graduação em Direito Digital e Compliance,
               com especialização em Proteção de Dados e Privacidade e certificação internacional EXIN.
               Iniciei a carreira profissional na área tecnologia de informação, ministrando aulas de informática,
               posteriormente cursei direito realizando estágio no Ministério Púplico do Estado de São Paulo
               e depois trabalhei com restituição de impostos;
-              Atualmente sou professor de informática para concursos públicos, ajudando diversos alunos a alcançar um cargo público.
-              Segundo metodologia DISC, tenho um perfil sociável e comunicativo, gosto de estar em contato com as pessoas, sou conservador e diplomático.
-              </h4>
-              <br />
-              <Button
-                color="danger"
-                size="lg"
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fas fa-play" />
-                Cronologia da carreira
-              </Button>
-            </GridItem>
-          </GridContainer>
-        </div>
-      </Parallax>
-      <div className={classNames(classes.main, classes.mainRaised)}>
-        <div className={classes.container}>
-          <ProductSection />
-          <TeamSection />
-          <WorkSection />
+              Atualmente sou professor de informática para concursos públicos, 
+              ajudando diversos alunos a alcançar um cargo público.
+              Segundo metodologia DISC, tenho um perfil sociável e comunicativo, 
+              gosto de estar em contato com as pessoas, sou conservador e diplomático.{" "}
+              </p>
+            </div>
+
+          </div>
         </div>
       </div>
       <Footer />
