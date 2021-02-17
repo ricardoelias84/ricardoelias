c214ff2de99065f71c11ef82acca64d6e39c6a59
ricardoelias84 <ricardoelias@gmail.com>
2021-02-17 14:35:14 -0300

    alterando section

--- pages-sections/Components-Sections/SectionBasics.js
+++ pages-sections/Components-Sections/SectionBasics.js
@@ -71,67 +71,8 @@ export default function SectionBasics() {
   return (
     <div className={classes.sections}>
       <div className={classes.container}>
-        <div className={classes.title}>
-          <h2>Basic Elements</h2>
-        </div>
-        <div id="buttons">
-          <div className={classes.title}>
-            <h3>
-              Buttons
-              <br />
-              <small>Pick your style</small>
-            </h3>
-          </div>
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={12} md={8}>
-              <Button color="primary">Default</Button>
-              <Button color="primary" round>
-                round
-              </Button>
-              <Button color="primary" round>
-                <Favorite className={classes.icons} /> with icon
-              </Button>
-              <Button justIcon round color="primary">
-                <Favorite className={classes.icons} />
-              </Button>
-              <Button color="primary" simple>
-                simple
-              </Button>
-            </GridItem>
-          </GridContainer>
-          <div className={classes.title}>
-            <h3>
-              <small>Pick your size</small>
-            </h3>
-          </div>
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={12} md={8}>
-              <Button color="primary" size="sm">
-                Small
-              </Button>
-              <Button color="primary">Regular</Button>
-              <Button color="primary" size="lg">
-                Large
-              </Button>
-            </GridItem>
-          </GridContainer>
-          <div className={classes.title}>
-            <h3>
-              <small>Pick your color</small>
-            </h3>
-          </div>
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={12} md={8}>
-              <Button>Default</Button>
-              <Button color="primary">Primary</Button>
-              <Button color="info">Info</Button>
-              <Button color="success">Success</Button>
-              <Button color="warning">Warning</Button>
-              <Button color="danger">Danger</Button>
-              <Button color="rose">Rose</Button>
-            </GridItem>
-          </GridContainer>
-        </div>
+      
+
         <div className={classes.space50} />
         <div id="inputs">
           <div className={classes.title}>
