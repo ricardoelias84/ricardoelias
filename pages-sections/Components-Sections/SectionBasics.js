de177dd3ce39cf41dbb26b8738fddf9a89b0c24c
ricardoelias84 <ricardoelias@gmail.com>
2021-02-19 05:57:17 -0300

    na madruga boladona3

--- pages-sections/Components-Sections/SectionBasics.js
+++ pages-sections/Components-Sections/SectionBasics.js
@@ -16,9 +16,11 @@ import FiberManualRecord from "@material-ui/icons/FiberManualRecord";
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
-
+import Button from "components/CustomButtons/Button.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
 import CustomLinearProgress from "components/CustomLinearProgress/CustomLinearProgress.js";
-
+import Paginations from "components/Pagination/Pagination.js";
+import Badge from "components/Badge/Badge.js";
 
 import styles from "assets/jss/nextjs-material-kit/pages/componentsSections/basicsStyle.js";
 
@@ -30,31 +32,7 @@ export default function SectionBasics() {
   const [selectedEnabled, setSelectedEnabled] = React.useState("b");
   const [checkedA, setCheckedA] = React.useState(true);
   const [checkedB, setCheckedB] = React.useState(false);
-  React.useEffect(() => {
-    if (
-      !document
-        .getElementById("sliderRegular")
-        .classList.contains("noUi-target")
-    ) {
-      Slider.create(document.getElementById("sliderRegular"), {
-        start: [40],
-        connect: [true, false],
-        step: 1,
-        range: { min: 0, max: 100 }
-      });
-    }
-    if (
-      !document.getElementById("sliderDouble").classList.contains("noUi-target")
-    ) {
-      Slider.create(document.getElementById("sliderDouble"), {
-        start: [20, 60],
-        connect: [false, true, false],
-        step: 1,
-        range: { min: 0, max: 100 }
-      });
-    }
-    return function cleanup() {};
-  });
+  
   const handleToggle = value => {
     const currentIndex = checked.indexOf(value);
     const newChecked = [...checked];
